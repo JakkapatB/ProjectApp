d1f6ce2b6121fd0a8d6f4db2fd5d5e2dc145de17
jakkapat <jakkapat.bo@kkumail.com>
2022-04-14 23:23:28 +0700

    add new version 14/4/65

--- MyProjectAppFlutter/Modern Farm/lib/incomeAndExpensesPage.dart
+++ MyProjectAppFlutter/Modern Farm/lib/incomeAndExpensesPage.dart
@@ -1,5 +1,7 @@
+import 'dart:ffi';
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,6 +10,7 @@ class incomeAndExpensesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    DateTime date = DateTime.now();
     return Scaffold(
       appBar: AppBar(
         title: Padding(
@@ -88,7 +91,6 @@ class incomeAndExpensesPage extends StatelessWidget {
                       Padding(
                         padding: EdgeInsets.fromLTRB(50, 200, 50, 0),
                         child: TextField(
-                            autofocus: true,
                             scrollPadding: EdgeInsets.all(10),
                             decoration: InputDecoration(
                                 labelText: "รายการ",
@@ -108,26 +110,45 @@ class incomeAndExpensesPage extends StatelessWidget {
                                         Radius.circular(20))))),
                       ),
                       Padding(
-                        padding: EdgeInsets.fromLTRB(50, 380, 50, 0),
-                        child: TextField(
-                            keyboardType: TextInputType.number,
-                            scrollPadding: EdgeInsets.all(10),
-                            decoration: InputDecoration(
-                                labelText: "วัน/เดือน/ปี",
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(20),
-                                  ),
-                                ))),
+                        padding: EdgeInsets.fromLTRB(60, 380, 50, 0),
+                        child: ElevatedButton(
+                          child: Text('เลือก วัน/เดือน/ปี'),
+                          onPressed: () async {
+                            DateTime? newDate = await showDatePicker(
+                              context: context,
+                              initialDate: date,
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime(2100),
+                            );
+                            if (newDate == null) return;
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xff3CB371),
+                            onPrimary: Colors.white,
+                            minimumSize: Size(400, 40),
+                          ),
+                        ),
                       ),
                       Padding(
-                        padding: EdgeInsets.fromLTRB(150, 500, 0, 0),
+                        padding: EdgeInsets.fromLTRB(60, 450, 0, 0),
                         child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                              primary: Color(0xff17352e),
-                              onPrimary: Colors.deepOrange.shade100),
+                              minimumSize: Size(165, 40),
+                              primary: Color(0xff5dd267),
+                              onPrimary: Colors.black87),
                           onPressed: () {},
-                          child: Text('บันทึก'),
+                          child: Text('รายรับ'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(60, 500, 0, 0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(165, 40),
+                              primary: Color(0xffff4167),
+                              onPrimary: Colors.black87),
+                          onPressed: () {},
+                          child: Text('รายจ่าย'),
                         ),
                       ),
                       Container(
--- MyProjectAppFlutter/Modern Farm/lib/main.dart
+++ MyProjectAppFlutter/Modern Farm/lib/main.dart
@@ -30,6 +30,7 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  DateTime date = DateTime(2022, 12, 24);
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
