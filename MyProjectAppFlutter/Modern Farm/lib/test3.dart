f8e089c17447b9bce0a83645b8663f7e88360521
thitinan <thitinan.ph@kkumail.com>
2022-04-24 18:39:31 +0700

    add 2

--- MyProjectAppFlutter/Modern Farm/lib/test3.dart
+++ MyProjectAppFlutter/Modern Farm/lib/test3.dart
@@ -6,55 +6,46 @@ class test3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Color(0xFFABD793),
-        leading: IconButton(
-          icon: Icon(Icons.ac_unit_sharp),
-          onPressed: () {},
-        ),
-        title: const Text('วิธีใช้งาน',
-            style: TextStyle(
-                color: Color(0xFF17352E),
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold)),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
-            },
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Color(0xFFABD793),
+          leading: IconButton(
+            icon: Icon(Icons.ac_unit_sharp),
+            onPressed: () {},
           ),
-          IconButton(
-            icon: const Icon(Icons.navigate_next),
-            tooltip: 'Go to the next page',
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute<void>(
-                builder: (BuildContext context) {
-                  return Scaffold(
-                    appBar: AppBar(
-                      title: const Text('Next page'),
-                    ),
-                    body: const Center(
-                      child: Text(
-                        'This is the next page',
-                        style: TextStyle(fontSize: 24),
+          title: const Text('วิธีใช้งาน',
+              style: TextStyle(
+                  color: Color(0xFF17352E),
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold)),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.account_circle_sharp),
+              tooltip: 'Go to the next page',
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute<void>(
+                  builder: (BuildContext context) {
+                    return Scaffold(
+                      appBar: AppBar(
+                        title: const Text('Next page'),
                       ),
-                    ),
-                  );
-                },
-              ));
-            },
-          ),
-        ],
-      ),
+                      body: const Center(
+                        child: Text(
+                          'This is the next page',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                      ),
+                    );
+                  },
+                ));
+              },
+            ),
+          ]),
       body: SingleChildScrollView(
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(8.0),
               child: Row(
                 children: [
                   Expanded(
@@ -68,29 +59,14 @@ class test3 extends StatelessWidget {
                       ),
                     ),
                   ),
+                  Expanded(
+                    child: Text("testtt",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold)),
+                  ),
                 ],
               ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: FittedBox(
-                    fit: BoxFit.contain, // otherwise the logo will be tiny
-                    child: Image.network(
-                      "assets/images/031.png",
-                      // width: 2,
-                      // height: 1,
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Text("testtt",
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-                ),
-              ],
             ),
             Row(
               children: [
